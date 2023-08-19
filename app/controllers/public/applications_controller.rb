8ad49a1c8605dc0cab81f373c135727ce0af9394
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-08-19 20:50:06 +0000

    [Add]応募キャンセル後の流れの記述

--- app/controllers/public/applications_controller.rb
+++ app/controllers/public/applications_controller.rb
@@ -1,6 +1,6 @@
 class Public::ApplicationsController < ApplicationController
 before_action :authenticate_user!
-before_action :is_matching_login_user, only: [:cancel, :withdraw]
+# before_action :is_matching_login_user, only: [:cancel, :withdraw]
 
 def confirm
   @recruitment=Recruitment.find_by(id: params[:recruitment_id])
@@ -9,12 +9,17 @@ end
 
 def create
   @recruitment=Recruitment.find_by(id: params[:recruitment_id])
-  application=Application.new(application_params)
+  if @recruitment.applications.find_by(applicant_id: current_user.id).blank?
+  　application=Application.new(application_params)
   # application.save(recruitment_id: @recruitment.id, applicant_id: current_user.id)
-  application.recruitment_id=@recruitment.id
-  application.applicant_id=current_user.id
-  if application.save
-  # application.save(recruitment_id: params[:recruitment_id], applicant_id: current_user.id)
+ 　  application.recruitment_id=@recruitment.id
+  　application.applicant_id=current_user.id
+    application.save
+    redirect_to complete_recruitment_applications_path
+  elsif @recruitment.applications.find_by(applicant_id: current_user.id).is_valid==false
+    application=@recruitment.applications.find_by(applicant_id: current_user.id)
+    application.update(application_params)
+    application.update(is_valid: true)
     redirect_to complete_recruitment_applications_path
   else
     render :confirm
@@ -45,7 +50,7 @@ def withdraw
   # application.update(is_valid: false)
 
   application.update(is_valid: false)
-  flash[:notice]="のキャンセルが完了しました。"
+  flash[:notice]="応募のキャンセルが完了しました。"
   redirect_to history_applications_path
   # redirect_to recruitment_applications_path(recruitment.id)
 end
@@ -57,12 +62,12 @@ private
     params.require(:application).permit(:message, :schedule_one, :schedule_two, :schedule_three)
   end
 
-  def is_matching_login_user
-    recruitment=Recruitment.find_by(id: params[:recruitment_id])
-    unless recruitment.applicant_id == current_user.id
-      redirect_to recruitments_path
-    end
-  end
+  # def is_matching_login_user
+  #   recruitment=Recruitment.find_by(id: params[:recruitment_id])
+  #   unless recruitment.applicant_ids == current_user.id
+  #     redirect_to recruitments_path
+  #   end
+  # end
   
 end
 
