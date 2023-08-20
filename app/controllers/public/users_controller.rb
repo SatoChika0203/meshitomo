41e8c398dcbc7ef299be9b357e850a63178da084
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-08-20 04:30:01 +0000

    [Add]ユーザー退会時のアソシエーションの削除機能追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,16 +2,16 @@ class Public::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :is_matching_login_user, only: [:edit, :update, :confirm, :withdraw]
   before_action :is_deleted_user, only: [:show]
-  
+
 
   def show
     @user=User.find(params[:id])
     @recruitments = Recruitment.where(user_id: @user.id)
-    
+
     @recruitments_male_only=@recruitments.where(recruitment_gender: 0).or(Recruitment.where(recruitment_gender: 2)).page(params[:page])
     @recruitments_female_only=@recruitments.where(recruitment_gender: 1).or(Recruitment.where(recruitment_gender: 2)).page(params[:page])
     @recruitments_anyone=@recruitments.where(recruitment_gender: 2).page(params[:page])
-    
+
     @recruitments_male_only=@recruitments.where(recruitment_gender: 0).or(Recruitment.where(recruitment_gender: 2)).order(created_at: :desc).page(params[:page])
     @recruitments_female_only=@recruitments.where(recruitment_gender: 1).or(Recruitment.where(recruitment_gender: 2)).order(created_at: :desc).page(params[:page])
     @recruitments_anyone=@recruitments.where(recruitment_gender: 2).order(created_at: :desc).page(params[:page])
@@ -37,8 +37,15 @@ class Public::UsersController < ApplicationController
   end
 
   def withdraw
+    ActiveRecord::Base.transaction do
     current_user.update(is_deleted: true)
     # is_deletedカラムをtrueにupdateする事により、退会状態を作り出す
+    current_user.applications.destroy
+    current_user.chats.destroy
+    current_user.chat_group_users.destroy
+    current_user.shops.destroy
+    current_user.recruitments.update(is_valid: false)
+    end
     reset_session
     redirect_to root_path
   end
@@ -49,14 +56,14 @@ private
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :postal_code, :address, :telephone_number, :email, :encrypted_password, :gender, :prefecture, :introduction, :is_deleted, :image)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to recruitments_path
     end
   end
-  
+
   def is_deleted_user
     user = User.find(params[:id])
     if user.is_deleted == true
