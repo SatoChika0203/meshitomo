d51c7b98748d535a313034b1be11b9c05d6b35eb
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-08-25 13:05:56 +0000

    [Add]adminの調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,5 +3,9 @@ class Public::HomesController < ApplicationController
     if user_signed_in?
       redirect_to recruitments_path
     end
+    
+    if admin_signed_in?
+      redirect_to admin_recruitments_path
+    end
   end
 end
