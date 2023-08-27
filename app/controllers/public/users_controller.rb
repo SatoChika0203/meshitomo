580a98e7b3485eb109ae9ddf5d181be7a9e33535
EC2 Default User <ec2-user@ip-172-31-1-135.ap-northeast-1.compute.internal>
2023-08-27 13:36:46 +0000

    [Add]Userコントローラーの変更（show）

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,12 +8,12 @@ class Public::UsersController < ApplicationController
     @user=User.find(params[:id])
     @recruitments = Recruitment.where(user_id: @user.id)
 
-    @recruitments_male_only=Recruitment.where(recruitment_gender: 0, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2)).page(params[:page])
-    @recruitments_female_only=Recruitment.where(recruitment_gender: 1, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2)).page(params[:page])
+    @recruitments_male_only=Recruitment.where(recruitment_gender: 0, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2, user_id: @user.id)).page(params[:page])
+    @recruitments_female_only=Recruitment.where(recruitment_gender: 1, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2, user_id: @user.id)).page(params[:page])
     @recruitments_anyone=Recruitment.where(recruitment_gender: 2, user_id: @user.id).page(params[:page])
 
-    @recruitments_male_only=Recruitment.where(recruitment_gender: 0, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2)).order(created_at: :desc).page(params[:page])
-    @recruitments_female_only=Recruitment.where(recruitment_gender: 1, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2)).order(created_at: :desc).page(params[:page])
+    @recruitments_male_only=Recruitment.where(recruitment_gender: 0, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2, user_id: @user.id)).order(created_at: :desc).page(params[:page])
+    @recruitments_female_only=Recruitment.where(recruitment_gender: 1, user_id: @user.id).or(Recruitment.where(recruitment_gender: 2, user_id: @user.id)).order(created_at: :desc).page(params[:page])
     @recruitments_anyone=Recruitment.where(recruitment_gender: 2, user_id: @user.id).order(created_at: :desc).page(params[:page])
   end
 
